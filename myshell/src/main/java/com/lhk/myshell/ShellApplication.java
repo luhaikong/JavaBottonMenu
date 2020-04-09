e5c9944834143b9f0e8526662c61cf4b36f59255
lihongkun <1031350293@qq.com>
2020-04-09 17:47:17 +0800

    Initial commit

---
+++ myshell/src/main/java/com/lhk/myshell/ShellApplication.java
@@ -0,0 +1,12 @@
+package com.lhk.myshell;
+
+import android.app.Application;
+import android.content.Context;
+
+public class ShellApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+}
